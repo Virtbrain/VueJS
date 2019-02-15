34518bbfc5f34ab148beffaa3584d85511945fd1
AlexeyManankov <virtbrain@gmail.com>
2019-02-15 18:21:22 +0500

    v6.4

--- Homeproj6/v3/script.js
+++ Homeproj6/v3/script.js
@@ -5,27 +5,43 @@ new Vue({
             {
                 plot: "Сегодня я разбил машину!",
                 writer: 'Aлекс',
+                upvotes: 28
             },
             {
                 plot: "Вчера кто-то украл мою сумку!",
                 writer: 'Джон',
+                upvotes: 8
             },
             {
                 plot: "Кто-то съел мой шоколад!",
                 writer: 'Джон',
+                upvotes: 51
             },
             {
                 plot: "Я съел чей-то шоколад",
                 writer: 'Aлекс',
+                upvotes:74
             },
-        ]
+        ],
+        query: ' '
+    },
+    computed: {
+        famous: function() {
+            return this.stories.filter(function(item){
+                return item.upvotes >25;
+            })
+        },
+        search: function() {
+            var query = this.query;
+            return this.stories.filter(story => story.plot.includes(query))
+        }
     },
     methods: {
         //method to filter stories by author
         storiesBy: function(writer) {
             return this.stories.filter(function (story) {
                 return story.writer === writer
-            })
+            });
         },
     }
 })
